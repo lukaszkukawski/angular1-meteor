b19417b0d36793c24a0e4d73c48d01d9403bb15c
kuka <kukawski.lukasz@gmail.com>
2015-11-05 05:31:17 +0100

    0. Bootstrapping

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # angular1-meteor
+My example from http://angular-meteor.com/
---
+++ app.js
@@ -0,0 +1,4 @@
+/*globals Meteor,angular*/
+if (Meteor.isClient) {
+    angular.module('socially', ['angular-meteor']);
+}
