29858556059aa8a2c35115d31cd5aca8a9833284
kuka <kukawski.lukasz@gmail.com>
2015-11-06 06:14:20 +0100

    6. Bind one object

--- app.ng.js
+++ app.ng.js
@@ -33,8 +33,20 @@ if (Meteor.isClient) {
         };
     });
 
-    angular.module('socially').controller('PartyDetailsCtrl', function ($scope, $stateParams) {
+    angular.module('socially').controller('PartyDetailsCtrl', function ($scope, $stateParams, $meteor) {
         'use stict';
-        $scope.partyId = $stateParams.partyId;
+        $scope.party = $meteor.object(Parties, $stateParams.partyId, false);
+
+        $scope.save = function () {
+            $scope.party.save().then(function (id) {
+                console.log('save success doc affected ', id);
+            }, function (error) {
+                console.log('save error', error);
+            });
+        };
+
+        $scope.reset = function () {
+            $scope.party.reset();
+        };
     });
 }
