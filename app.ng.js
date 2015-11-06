0b0825d9898fe62a8432145ddd55ff721d060f2d
kuka <kukawski.lukasz@gmail.com>
2015-11-06 05:58:12 +0100

    5. Routing & Multiple Views

--- app.ng.js
+++ app.ng.js
@@ -1,7 +1,24 @@
 /*globals Meteor,angular, Mongo, Parties*/
 
 if (Meteor.isClient) {
-    angular.module('socially', ['angular-meteor']);
+    angular.module('socially', ['angular-meteor', 'ui.router']);
+
+    angular.module('socially').config(function ($urlRouterProvider, $stateProvider, $locationProvider) {
+        'use strict';
+        $locationProvider.html5Mode(true);
+        $stateProvider
+            .state('parties', {
+                url: '/parties',
+                templateUrl: 'parties-list.ng.html',
+                controller: 'PartiesListCtrl'
+            })
+            .state('partyDetails', {
+                url: '/parties/:partyId',
+                templateUrl: 'party-details.ng.html',
+                controller: 'PartyDetailsCtrl'
+            });
+        $urlRouterProvider.otherwise('/parties');
+    });
 
     angular.module('socially').controller('PartiesListCtrl', function ($scope, $meteor) {
         'use strict';
@@ -13,6 +30,11 @@ if (Meteor.isClient) {
 
         $scope.removeAll = function () {
             $scope.parties.remove();
-        }
+        };
+    });
+
+    angular.module('socially').controller('PartyDetailsCtrl', function ($scope, $stateParams) {
+        'use stict';
+        $scope.partyId = $stateParams.partyId;
     });
 }
