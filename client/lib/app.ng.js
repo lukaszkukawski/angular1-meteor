a38ca39514998516b0c0602798cf88f4958c2187
kuka <kukawski.lukasz@gmail.com>
2015-11-08 16:03:42 +0100

    11. Running your app on Android or iOS with PhoneGap

--- client/lib/app.ng.js
+++ client/lib/app.ng.js
@@ -1,3 +1,16 @@
 /*globals Meteor,angular, Mongo, Parties*/
 
 angular.module('socially', ['angular-meteor', 'ui.router']);
+
+function onReady() {
+    'use strict';
+    angular.bootstrap(document, ['socially'], {
+        strictDi: true
+    });
+}
+
+if (Meteor.isCordova) {
+    angular.element(document).on('deviceready', onReady);
+} else {
+    angular.element(document).ready(onReady);
+}
